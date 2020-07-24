fc410db2e5da73706f8d52996fea67f44b366fc9
liangsen <1098709715@qq.com>
2020-07-24 21:35:36 +0800

    退出登录

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>这里是home</h1>
+        <el-button type="info" @click="logout">退出登录</el-button>
+    </div>
+</template>
+<script>
+export default {
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
-
+import Home from '../components/Home.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -12,8 +12,24 @@ const router = new VueRouter({
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
   ]
 })
-
+// 路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 从哪儿来
+  // next() 放行
+  if (to.path === '/login') return next()
+  // 如果是去登录,就放行
+  const tokenStr = sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  // 如果没token就跳转去登录
+  next()
+  // 有token 就放行
+})
 export default router
